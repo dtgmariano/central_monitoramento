19f5646484e8e78b641d393fca9334727bec5dbb
andreinakagawa <andreinakagawa@outlook.com>
2013-10-23 20:01:13 -0200

    updated icu center interface

--- testplace/centgui2.py
+++ testplace/centgui2.py
@@ -8,6 +8,7 @@ class myWindow(QWidget):
 		QWidget.__init__(self)
 		self.ui = Ui_Form()
 		self.ui.setupUi(self)
+		self.ui.scrolll.setStyleSheet("background-color:black;");
 		QObject.connect(self.ui.btnNew, SIGNAL('clicked()'), self.Create)	
 		QObject.connect(self.ui.btnClose, SIGNAL('clicked()'), self.Close)	
 		self.obj = None	
@@ -17,14 +18,17 @@ class myWindow(QWidget):
 		self.obj = MyMonitor(self)
 		#self.obj.setGeometry(500, 500, 550, 550)
 		#self.obj.show()
-		self.ui.graficos.addWidget(self.obj)
-		self.ui.graficos.setRowMinimumHeight(len(self.objlist),256)
+		row = len(self.objlist)/2
+		column = 0 if len(self.objlist)%2 == 0 else 1
+		self.ui.graficos.addWidget(self.obj,row, column)
+		self.ui.graficos.setRowMinimumHeight(row,180)
 		self.objlist.append(self.obj)
 		print self.objlist
 		
 	def Close(self):
-		self.objlist.pop().close()		
-		
+		obj = self.objlist.pop()
+		self.ui.graficos.removeWidget(obj)
+		obj.deleteLater()
 if __name__ == "__main__":
 	import sys
 	app = QApplication([])
