2897a2bff5d4780203323b77c8919c6e32944b87
andreinakagawa <andreinakagawa@outlook.com>
2013-10-25 20:21:47 -0200

    interface for alarm config

---
+++ gui/alarms.py
@@ -0,0 +1,16 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from alarms_gui import Ui_AlarmsForm
+
+class AlarmForm(QWidget):
+	def __init__(self, parent = None):
+		super(AlarmForm, self).__init__(parent)
+		self.ui = Ui_AlarmsForm()
+		self.ui.setupUi(self)
+
+if __name__ == "__main__":
+	import sys
+	app = QApplication(sys.argv)
+	wind = AlarmForm()
+	wind.show()
+	app.exec_()
